7742a503f37c1797a1898dbdd46fa80946faaffa
my4fdal <my4fdal@gmail.com>
2020-01-21 01:12:55 +0700

    update

--- create_sertifikat.py
+++ create_sertifikat.py
@@ -3,19 +3,20 @@ import requests
 host = "http://localhost/create_sertifikat/index.php"
 
 def struktur() :
-    data = """AFDAL:KOORDINATOR PUBLIKASI DAN DOKUMENTASI,SELVILA YOLANDA:STAFF KOORDINATOR PUBLIKASI DAN DOKUMENTASI,RIFA ZURMAERA:STAFF KOORDINATOR PUBLIKASI DAN DOKUMENTASI,VIVI RAHMAWATI:SEKRETARIS 1,HAFIZA AULIA:SEKRETARIS UMUM,ERMAN:KOORDINATOR PERLENGKAPAN,RIRI SILVANI DWI PUTRI: BENDAHARA 1,YOVI REZKI PUTRA: KETUA UMUM JAWARA PNP,ILHAM PRASETIYA:WAKIL KETUA UMUM JAWARA PNP,MUHAMAD AZRI : STAFF KOORDINATOR PSDM,RIZKA HIDAYAT : BENDAHARA UMUM,ANWADI LAMARCHTA : KOORDINATOR HUMAS,ZIKRI : STAFF KOORDINATOR HUMAS, MILIO ONER:KOORDINATOR PSDM,M. IKHSAN MUZZAKI:KETUA CABOR KARATE"""
+    data = """Afdal:Koordinator Publikasi Dan Dokumentasi,Selvila Yolanda:Staff Koordinator Publikasi Dan Dokumentasi,Rifa Zurmaera:Staff Koordinator Publikasi Dan Dokumentasi,Vivi Rahmawati:Sekretaris 1,Hafiza Aulia:Sekretaris Umum,Erman:Koordinator Perlengkapan,Riri Silvani Dwi Putri: Bendahara 1,Yovi Rezki Putra: Ketua Umum Jawara Pnp,Ilham Prasetiya:Wakil Ketua Umum Jawara Pnp,Muhamad Azri : Staff Koordinator Psdm,Rizka Hidayat : Bendahara Umum,Anwadi Lamarchta : Koordinator Humas,Zikri : Staff Koordinator Humas, Milio Oner:Koordinator Psdm,M. Ikhsan Muzzaki:Ketua Cabor Karate"""
     data = data.split(",")
     for value in data :
         value = value.split(":")
         (nama, jabatan) = value 
-        send = requests.post(host, data={'nama':nama, 'jabatan':jabatan})
+        send = requests.post(host, data={'nama':nama, 'jabatan':jabatan, 'dir':'Struktur'})
         print(send.text)
 
 def anggota():
-    data = """Afdhallurijal, Ahmad Affandi, Akmal Majid, Anggit Demariza, Anwadi Lamarchta, Arif Irsyad, Dessy Kashariyanti, Elsy Krisdayanti, Erman, Eqrio Gravinda Mafi, Fadly Indri Jasaputra, Hafiza Aulia, Harun Lubis, Jimmy Ramadan, M.Isthofa Ardhana, M.Syukri, Michelle Novri C.P, Milio Oner, Musa Korneles S., Nilam,Azizah, Nurul Annisa, Nurul Utami Husna, Rahmat Putra Zeep, Rahmianti, Raju Nofrial, Rangga Syaputra, Rizka Hidayat, Sukma Yulhamdani, Yovi Rezki Putra, Yulistiawati"""
+    data = """Afdhallurijal,Ahmad Affandi,Akmal Majid,Anggit Demariza,Anwadi Lamarchta,Arif Irsyad, Dessy Kashariyanti,Elsy Krisdayanti,Erman,Eqrio Gravinda Mafi,Fadly Indri Jasaputra,Hafiza Aulia,Harun Lubis, Jimmy Ramadan,M.Isthofa Ardhana,M.Syukri,Michelle Novri C.P,Milio Oner, Musa Korneles S.,Nilam,Azizah, Nurul Annisa,Nurul Utami Husna,Rahmat Putra Zeep,Rahmianti,Raju Nofrial,Rangga Syaputra,Rizka Hidayat, Sukma Yulhamdani,Yovi Rezki Putra,Yulistiawati"""
     data = data.split(',')
     for value in data :
-        send = requests.post(host, data={'nama': value, 'jabatan': 'ANGGOTA UKM JAWARA'})
+        send = requests.post(
+            host, data={'nama': value, 'jabatan': 'Anggota Ukm Jawara', 'dir': 'Anggota'})
         print(send.text)
 
 anggota()
--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 require_once('MyLibraries/AddImageText.php');
 
-function addSertifikat($nama, $jurusan){
+function addSertifikat($nama, $jurusan, $dir){
     $nama = ucwords($nama);
     $jurusan = ucwords($jurusan);
-    $dir = "ListSertifikat/{$nama}.jpg" ;
+    $dir = "ListSertifikat/{$dir}/{$nama}.jpg" ;
 
     $img = new AddImageText(__DIR__.'\Sertifikat\sertifikat.jpg');
     $img->setTextSize(120);
@@ -18,27 +18,7 @@ function addSertifikat($nama, $jurusan){
 if($_SERVER['REQUEST_METHOD'] = "POST"){
     $nama = $_POST["nama"];
     $jabatan = $_POST["jabatan"];
-    addSertifikat($nama, $jabatan);
+    $dir = $_POST["dir"];
+    addSertifikat($nama, $jabatan, $dir);
     echo json_encode($_POST);
-}
-
-function struktur(){
-    $data = "AFDAL:KOORDINATOR PUBLIKASI DAN DOKUMENTASI,
-    SELVILA YOLANDA:STAFF KOORDINATOR PUBLIKASI DAN DOKUMENTASI,
-    RIFA ZURMAERA:STAFF KOORDINATOR PUBLIKASI DAN DOKUMENTASI,
-    VIVI RAHMAWATI:SEKRETARIS 1,
-    HAFIZA AULIA:SEKRETARIS UMUM,
-    ERMAN:KOORDINATOR PERLENGKAPAN,
-    RIRI SILVANI DWI PUTRI: BENDAHARA 1,
-    YOVI REZKI PUTRA: KETUA UMUM JAWARA PNP,
-    ILHAM PRASETIYA:WAKIL KETUA UMUM JAWARA PNP,
-    MUHAMAD AZRI : STAFF KOORDINATOR PSDM,
-    RIZKA HIDAYAT : BENDAHARA UMUM, 
-    ANWADI LAMARCHTA : KOORDINATOR HUMAS
-    ZIKRI : STAFF KOORDINATOR HUMAS";
-    $data = explode(",", $data);
-    foreach ($data as $key => $value) {
-        $value = explode(":", $value);
-        addSertifikat($value[0], $value[1]);
-    }
 }
